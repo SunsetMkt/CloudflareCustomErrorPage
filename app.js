60032ac5431cbffe6be6e83de0c91ace1d8dff7c
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-09-17 21:59:29 +0800

    chore: ignore

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const ejs = require("ejs");
 const configs = [require("./config.js"), require("./i18n/config-zh-CN.js")];
 const fs = require("fs");
+
+if (!fs.existsSync("./public")) {
+  fs.mkdirSync("./public");
+}
+fs.copyFileSync("./index.css", "./public/index.css");
+
 configs.forEach((config) =>
   config.builderConfig.map((item) => {
     let html = ejs.render(
